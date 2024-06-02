901c5279bd6fba1e96d5782fefee72141f575ccd
SteffenLee <tnqjae@naver.com>
2024-06-02 16:18:24 +0900

    Edit code

--- lib/etc/SplashScreen.dart
+++ lib/etc/SplashScreen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'LoginScreen.dart';
+import '../AboutLogin/LoginScreen.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
